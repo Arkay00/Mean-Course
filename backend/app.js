b389c46f44f5616c830ec661a73a23693f519ebe
Arkay00 <robin.kaufmann@gmx.net>
2024-09-04 19:59:17 +0200

    39

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first Middleware')
-    next();
-})
-
-app.use((req, res, next) => {
-    res.send('Hello from Express');
-})
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: '123445',
+            title: 'First server post',
+            content: 'This comes from the server'
+        },
+        {
+            id: '1234456',
+            title: 'Second server post',
+            content: 'This comes from the server,too'
+        }
+    ];
+    // return
+    res.status(200).json({
+        message: 'Posts send successfully', 
+        posts: posts
+    });
+;})
 
 module.exports = app;
