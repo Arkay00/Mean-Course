b3c0738ff764b683317ff19a90e2c0b5c950e740
Arkay00 <robin.kaufmann@gmx.net>
2024-09-07 20:56:03 +0200

    40 and fixed error in 39 (wrong port 300 used in service)

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+// handles CORS error
+// Control witch methods may be send
+app.use((req, res, next)  => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With,Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, PUT, OPTIONS");
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {
