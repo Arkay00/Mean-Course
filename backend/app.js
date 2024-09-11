d8932165721334dbc61e905a57f82d54f4d4d7c5
Arkay00 <robin.kaufmann@gmx.net>
2024-09-11 16:08:53 +0200

    52

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+
 const app = express();
 
 // needs body-parser package
@@ -19,7 +21,11 @@ app.use((req, res, next)  => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
+    // const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
